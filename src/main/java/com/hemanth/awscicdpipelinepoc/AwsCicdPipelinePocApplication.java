598c7e6494fb94f7577f54ef0fb24b353c9cc38f
hemanthcse1 <hemanthcse1@gmail.com>
2024-01-02 20:22:24 +0530

    Initial commit

---
+++ src/main/java/com/hemanth/awscicdpipelinepoc/AwsCicdPipelinePocApplication.java
@@ -0,0 +1,13 @@
+package com.hemanth.awscicdpipelinepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCicdPipelinePocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsCicdPipelinePocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hemanth/awscicdpipelinepoc/AwsCicdPipelinePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemanth.awscicdpipelinepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCicdPipelinePocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
